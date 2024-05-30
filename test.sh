71aa80c80f5a814ea5d45d141b4626c615857bc3
Josephine Westermann <josephine.westermann@uni-heidelberg.de>
2024-05-30 15:01:59 +0200

    Removed now redundant test.sh

--- test.sh
+++
@@ -1,9 +0,0 @@
-# if the script was called with -c, clean up existing data
-[[ "$@" =~ "-c" ]] && rm data.db
-
-python Database.py   || exit
-python Forward.py    || exit
-python Densities.py  || exit
-python MultiIndex.py || exit
-python Surrogates.py || exit
-python Transport.py  || exit
