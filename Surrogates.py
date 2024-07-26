b4683011130e2d25c6b8f5fe6bb9a57dda829540
Josephine Westermann <josephine.westermann@uni-heidelberg.de>
2024-07-26 15:45:18 +0200

    Refactored Surrogate: boolean flags for resampling/cond, removed domain, cleaned up printouts, etc

--- Surrogates.py
+++ Surrogates.py
@@ -11,8 +11,8 @@ import Evaluation as ev
 class Legendre :
     """ Surrogate $\\tilde f$ of a (unnormalized density) function $f : [-1,1]^d \\to [0,\\infty)$. """
 
-    def __init__(self, *, target: de.TargetDensity, multis: mi.MultiIndexSet,
-                 mode='wls', dist='cheby', verbose=0, save=False, domain=None) :
+    def __init__(self, *, target: de.TargetDensity, multis: mi.MultiIndexSet, mode='wls', dist='cheby',
+                 resample=False, compute_condnr=False, verbose=0, save=False) :
         """
         Parameters:
         -----------
@@ -38,56 +38,54 @@ class Legendre :
             - 'christoffel' : Random points distributed according to the Christoffel measure.
                               Construction is computationally expensive.
             - 'uni' : Uniformly distributed points, not recommended.
-
+        resample : bool
+            Specifies whether to resample the WLS points until the corresponding Gramian matrix G satisfies ||G-I|| <= .5.
+        compute_cond : bool
+            Specifies whether to compute the condition number of G-I even if resample==False.
         """
-        if verbose > 0: print('\t SETUP Surrogate')
+        if verbose > 0: print('\t Surrogate / ', end='')
         util.require.equal(multis.dim, target.dim, 'multis.dim', 'target.dim')
         assert mode in ['ip', 'wls', 'ls']
         assert dist in ['leja', 'leggaus', 'cheby', 'cheby_ss', 'christoffel', 'uni']
         self.multis = multis
         self.target = target
         self.coeffs = None
+        self.cond_nr = -1
         self.dim = multis.dim
-        self.domain = domain
 
         if save :
             self.dbo, isnew = db.SurrogateDBO.get_or_create(
                 target=target.name, target_id=None if not hasattr(target, 'dbo') else target.dbo.id,
                 multis=multis.name, multis_id=multis.dbo.id, mode=mode, dist=dist)
             if not isnew and self.dbo.coeffs is not None :
+                if verbose > 0: print('loading / ', end='')
                 self.coeffs = db.fr_string(self.dbo.coeffs)
 
         if self.coeffs is None :
+            if verbose > 0: print('computing / ', end='')
             start = time.process_time()
 
             # Determine number of support nodes
-            n = multis.size()
-            if mode == 'wls' : n = 10 * n * int(np.log(n))
-            if mode == 'ls' :  n = n ** 2
+            n = self.multis.cardinality
+            if mode == 'wls': n = 10 * n * int(np.log(n))
+            if mode == 'ls':  n = n ** 2
 
-            # Determine support points and weights
-            points, weights = util.points.get_sample_points_and_weights(multis, dist, n)
-
-            # Compute LHS and RHS
-            lhs = util.legendre.evaluate_basis(points, multis, mode='old')
-            if domain is not None : points = util.points.scale(points.T, [[-1, 1]] * self.dim, domain.r).T
-            rhs = np.squeeze(target.evalSqrt(points))
-            if weights is not None:
-                for i in range(lhs.shape[0]) :
-                    lhs[i,:] *= weights[i]
-                    rhs[i]   *= weights[i]
+            # Setup linear system
+            lhs, rhs = self._setup_lhs_rhs(mode, dist, resample, n, verbose)
 
             # Solve linear system
             self.coeffs, _, _, _ = np.linalg.lstsq(lhs, rhs, rcond=None)
             ctime = time.process_time() - start
 
-            self.L = lhs
-            self.G = np.dot(self.L.T, self.L)/self.L.shape[0] - np.eye(multis.size())
-
             if save :
-                self.dbo.condnr = 0 if lhs.size == 0 else np.linalg.cond(lhs)
+                if compute_cond and self.cond == -1 :
+                    if lhs.size == 0 :
+                        self.cond = 0
+                    else :
+                        self.cond = np.linalg.cond(np.dot(lhs.T, lhs) / lhs.shape[0] - np.eye(multis.cardinality))
+                self.dbo.condnr = self.cond
                 self.dbo.ctime = ctime
-                self.dbo.nevals = points.shape[1]
+                self.dbo.nevals = n
                 self.dbo.coeffs = db.to_string(self.coeffs)
                 self.dbo.save()
 
@@ -98,13 +96,35 @@ class Legendre :
         # L2 norm squared of sqrt(f) and L1 norm of f wrt to the uniform measure
         self.norm = 2**(-self.target.dim) * self.norm_lebesgue
 
-        if verbose > 0: print('\t SETUP Surrogate DONE')
+        if verbose > 0: print('done.')
+
+    def _setup_lhs_rhs(self, mode, dist, resample, n, verbose) :
+        while True :
+
+            # Determine support points and weights
+            points, weights = util.points.get_sample_points_and_weights(self.multis, dist, n)
+
+            # Compute LHS and RHS
+            lhs = util.legendre.evaluate_basis(points, self.multis, mode='old')
+            rhs = np.squeeze(self.target.evalSqrt(points))
+            if weights is not None:
+                for i in range(lhs.shape[0]):
+                    lhs[i, :] *= weights[i]
+                    rhs[i] *= weights[i]
+
+            if resample:
+                # check matrix
+                G_I = np.dot(lhs.T, lhs) / lhs.shape[0] - np.eye(self.multis.cardinality)
+                self.cond = 0 if lhs.size == 0 else np.linalg.cond(lhs)
+                if verbose > 1 : print(f'\t\t cond = {self.cond}')
+                if self.cond <= .5 :
+                    break
+            else :
+                break
+        return lhs, rhs
 
     def evalSqrt(self, x) :
         x = util.points.ensure_shape(x, self.dim)
-        if self.domain is not None :
-            # assert x in self.domain
-            x = util.points.scale(x.T, self.domain.r, [[-1, 1]] * self.dim).T
         basis = util.legendre.evaluate_basis(x, self.multis)
         return np.dot(basis, self.coeffs)
 
